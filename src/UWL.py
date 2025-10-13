ccb1bf63e73fef32972b07f7e474144c7591d2a2
Ronan LE PRAT <ronan.leprat@laposte.net>
2025-10-13 16:42:43 +0200

    One warehouse heuristic

--- src/UWL.py
+++ src/UWL.py
@@ -32,9 +32,31 @@ class UWL:
         self.open_warehouses = np.zeros(m)
         self.open_warehouses[0] = 1
         self.assignated_warehouses = np.zeros(n)
+        self.best_cost = self.compute_best_cost()
 
     def compute_best_cost(self):
-        return np.sum(self.build_costs * self.open_warehouses) * np.sum(self.distance_matrix[np.arange(self.n), self.assignated_warehouses.astype(int)])
+        return np.sum(self.build_costs * self.open_warehouses) + np.sum(self.distance_matrix[np.arange(self.n), self.assignated_warehouses.astype(int)])
+
+    def compute_cost(self, open_warehouses, assignated_warehouses):
+        return np.sum(self.build_costs * open_warehouses) + np.sum(self.distance_matrix[np.arange(self.n), assignated_warehouses.astype(int)])
+
+    def heuristic_one_warehouse(self):
+        
+        open_warehouses = np.zeros(self.m)
+        assignated_warehouses = np.zeros(self.n) - 1
+
+        for i in range(self.m):
+            open_warehouses[i-1] = 0
+            open_warehouses[i] = 1
+            assignated_warehouses = assignated_warehouses + 1
+            
+            new_cost = self.compute_cost(open_warehouses,assignated_warehouses)
+
+            if new_cost < self.best_cost:
+                print(new_cost)
+                self.open_warehouses = open_warehouses
+                self.assignated_warehouses = assignated_warehouses
+                self.best_cost = new_cost
 
     def print(self):
         print(f"""---------------------------
@@ -42,5 +64,5 @@ Uncapacitated warehouse location problem:
 Number of warehouses: {self.m}
 Number of clients: {self.n}
 Current best warehouse assignation: {self.assignated_warehouses}
-Current best cost: {self.compute_best_cost()}
+Current best cost: {self.best_cost}
 ---------------------------""")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from UWL import UWL
 
 def main():
     uwl = UWL("./instances/cap71.txt")
+    uwl.heuristic_one_warehouse()
     uwl.print()
 
 if __name__ == '__main__':
